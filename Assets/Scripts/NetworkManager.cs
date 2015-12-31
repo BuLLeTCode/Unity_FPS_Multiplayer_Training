47be75c6b20ed5992b8532133581fe2d3071330b
BuLLeT1331 <gta122@inbox.lv>
2015-12-31 19:25:15 +0200

    Photon Multiplayer base and very simple spawn system.
    Before new year! :D

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+
+    const string VERSION = "1.0";
+    public string roomName = "Room 69";
+
+    GameObject globalScriptsObject;
+
+    GameObject gameMapCamera;
+
+    Transform[] spawnPoints;
+    SpawnSystem spawnPointsFromSpawnSystem;
+
+    public Transform spawnPosition;//This is for now, spawn system will come...
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Sakums!");
+        PhotonNetwork.ConnectUsingSettings(VERSION);
+
+        gameMapCamera = GameObject.Find("Waiting_Camera");
+        globalScriptsObject = GameObject.Find("_GlobalScripts");
+
+        spawnPointsFromSpawnSystem = globalScriptsObject.GetComponent<SpawnSystem>();
+
+        spawnPoints = spawnPointsFromSpawnSystem.possibleSpawnPoints;
+        
+	}
+
+    public virtual void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster()");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby()");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public virtual void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("OnPhotonRandomJoinFailed()");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { maxPlayers = 4 }, null);
+    }
+
+    // the following methods are implemented to give you some context. re-implement them as needed.
+
+    public virtual void OnFailedToConnectToPhoton(DisconnectCause cause)
+    {
+        Debug.LogError("Cause: " + cause);
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom()");
+
+        spawnPlayer();
+    }
+
+    void spawnPlayer()
+    {
+
+        if (spawnPoints == null)
+        {
+            PhotonNetwork.Instantiate("PlayerController", spawnPosition.position, Quaternion.identity, 0);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate("PlayerController", spawnPoints[Random.Range(0, spawnPoints.Length)].position, spawnPoints[Random.Range(0, spawnPoints.Length)].rotation, 0);
+        }
+        
+
+        gameMapCamera.active = false;//When Player spawns no need for Map Camera.
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
---
+++ Assets/Scripts/SpawnSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSystem : MonoBehaviour {
+
+    private int teamID;
+    public Transform[] possibleSpawnPoints;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
